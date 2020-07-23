9ea9e005f0cc76097bafb5e9c98da310a80acfd9
SelmaKarapicak <karapicakselma@gmail.com>
2020-07-23 19:48:33 -0400

    smokedRunner added

--- target/cucumber-default-report/report.js
+++ target/cucumber-default-report/report.js
@@ -9,6 +9,25 @@ formatter.feature({
     }
   ]
 });
+formatter.background({
+  "name": "Given user logged in into HRMS",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user navigates to Add Employee page",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "com.hrms.steps.AddEmployee.user_navigates_to_Add_Employee_page()"
+});
+formatter.result({
+  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//a[@id\u003d\u0027menu_pim_addEmployee\u0027]\"}\n  (Session info: chrome\u003d84.0.4147.89)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027WINDOWS-8JREQPE\u0027, ip: \u0027192.168.1.117\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_241\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 84.0.4147.89, chrome: {chromedriverVersion: 84.0.4147.30 (48b3e868b4cc0..., userDataDir: C:\\Users\\karap\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:64168}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:virtualAuthenticators: true}\nSession ID: 5d0c89535733bbf55b96f25eec11145e\n*** Element info: {Using\u003dxpath, value\u003d//a[@id\u003d\u0027menu_pim_addEmployee\u0027]}\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat org.openqa.selenium.support.pagefactory.DefaultElementLocator.findElement(DefaultElementLocator.java:69)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:38)\r\n\tat com.sun.proxy.$Proxy17.getWrappedElement(Unknown Source)\r\n\tat org.openqa.selenium.remote.internal.WebElementToJsonConverter.apply(WebElementToJsonConverter.java:50)\r\n\tat java.util.stream.ReferencePipeline$3$1.accept(ReferencePipeline.java:193)\r\n\tat java.util.Spliterators$ArraySpliterator.forEachRemaining(Spliterators.java:948)\r\n\tat java.util.stream.AbstractPipeline.copyInto(AbstractPipeline.java:482)\r\n\tat java.util.stream.AbstractPipeline.wrapAndCopyInto(AbstractPipeline.java:472)\r\n\tat java.util.stream.ReduceOps$ReduceOp.evaluateSequential(ReduceOps.java:708)\r\n\tat java.util.stream.AbstractPipeline.evaluate(AbstractPipeline.java:234)\r\n\tat java.util.stream.ReferencePipeline.collect(ReferencePipeline.java:499)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.executeScript(RemoteWebDriver.java:484)\r\n\tat com.hrms.utils.CommonMethods.jsClick(CommonMethods.java:210)\r\n\tat com.hrms.steps.AddEmployee.user_navigates_to_Add_Employee_page(AddEmployee.java:29)\r\n\tat ✽.user navigates to Add Employee page(file:///C:/Users/karap/eclipse-workspace/HrmsCucumber/src/test/resources/features/AddAnEmployee.feature:5)\r\n",
+  "status": "failed"
+});
 formatter.scenario({
   "name": "Add Employee without employee id",
   "description": "",
@@ -22,9 +41,6 @@ formatter.scenario({
     }
   ]
 });
-formatter.before({
-  "status": "passed"
-});
 formatter.step({
   "name": "user logged in into HRMS",
   "keyword": "Given "
@@ -33,7 +49,7 @@ formatter.match({
   "location": "com.hrms.steps.AddEmployee.user_logged_in_into_HRMS()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "user navigates to Add Employee page",
@@ -43,7 +59,7 @@ formatter.match({
   "location": "com.hrms.steps.AddEmployee.user_navigates_to_Add_Employee_page()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "user enters employees first name and last name",
@@ -81,6 +97,7 @@ formatter.match({});
 formatter.result({
   "status": "undefined"
 });
+formatter.embedding("image/png", "embedded0.png", "Add Employee without employee id");
 formatter.after({
   "status": "passed"
 });
@@ -172,7 +189,8 @@ formatter.result({
   "status": "passed"
 });
 formatter.after({
-  "status": "passed"
+  "error_message": "org.openqa.selenium.NoSuchSessionException: Session ID is null. Using WebDriver after calling quit()?\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027WINDOWS-8JREQPE\u0027, ip: \u0027192.168.1.117\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_241\u0027\nDriver info: driver.version: RemoteWebDriver\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:125)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:609)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.getScreenshotAs(RemoteWebDriver.java:295)\r\n\tat com.hrms.utils.CommonMethods.takeScreenShot(CommonMethods.java:280)\r\n\tat com.hrms.steps.Hooks.end(Hooks.java:24)\r\n",
+  "status": "failed"
 });
 formatter.uri("file:src/test/resources/features/loginCredentials.feature");
 formatter.feature({
@@ -210,19 +228,22 @@ formatter.examples({
     {
       "cells": [
         "Username",
-        "Password"
+        "Password",
+        "FirstName"
       ]
     },
     {
       "cells": [
         "Admin",
-        "Hum@nhrm123"
+        "Hum@nhrm123",
+        "Admin"
       ]
     },
     {
       "cells": [
         "abd77",
-        "Syntax123!"
+        "Syntax123!",
+        "Abdullah"
       ]
     }
   ]
@@ -257,13 +278,14 @@ formatter.result({
   "status": "undefined"
 });
 formatter.step({
-  "name": "\"\u003cFirstName\u003e\" is successfully logged in",
+  "name": "\"Admin\" is successfully logged in",
   "keyword": "Then "
 });
 formatter.match({});
 formatter.result({
   "status": "undefined"
 });
+formatter.embedding("image/png", "embedded1.png", "Valid Login Credentials");
 formatter.after({
   "status": "passed"
 });
@@ -297,13 +319,14 @@ formatter.result({
   "status": "undefined"
 });
 formatter.step({
-  "name": "\"\u003cFirstName\u003e\" is successfully logged in",
+  "name": "\"Abdullah\" is successfully logged in",
   "keyword": "Then "
 });
 formatter.match({});
 formatter.result({
   "status": "undefined"
 });
+formatter.embedding("image/png", "embedded2.png", "Valid Login Credentials");
 formatter.after({
   "status": "passed"
 });
@@ -357,6 +380,7 @@ formatter.result({
   "error_message": "org.junit.ComparisonFailure: expected:\u003cInvalid [C]redentials\u003e but was:\u003cInvalid [c]redentials\u003e\r\n\tat org.junit.Assert.assertEquals(Assert.java:117)\r\n\tat org.junit.Assert.assertEquals(Assert.java:146)\r\n\tat com.hrms.steps.LoginWithInvalidCredentials.user_see_Invalid_credentials_error_message(LoginWithInvalidCredentials.java:31)\r\n\tat ✽.user see Invalid credentials error message(file:///C:/Users/karap/eclipse-workspace/HrmsCucumber/src/test/resources/features/loginWithInvalidCredentials.feature:8)\r\n",
   "status": "failed"
 });
+formatter.embedding("image/png", "embedded3.png", "Login with valid username and invalid password");
 formatter.after({
   "status": "passed"
 });
@@ -379,31 +403,4 @@ formatter.step({
 });
 formatter.match({
   "location": "com.hrms.steps.LoginWithInvalidCredentials.user_navigated_to_HRMS()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user enters valid username and empty password",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "com.hrms.steps.LoginWithInvalidCredentials.user_enters_valid_username_and_empty_password()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "user see Password can not be empty error message",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "com.hrms.steps.LoginWithInvalidCredentials.user_see_Password_can_not_be_empty_error_message()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
 });
